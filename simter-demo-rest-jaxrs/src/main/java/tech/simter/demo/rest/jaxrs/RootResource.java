fdfa83ee2f663e0331f6d04639c8e9d07e16c075
RJ.Hwang <rongjihuang@gmail.com>
2017-05-15 14:13:48 +0800

    Add Page test data in RootResource

--- simter-demo-rest-jaxrs/src/main/java/tech/simter/demo/rest/jaxrs/RootResource.java
+++ simter-demo-rest-jaxrs/src/main/java/tech/simter/demo/rest/jaxrs/RootResource.java
@@ -2,6 +2,10 @@ package tech.simter.demo.rest.jaxrs;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import java.awt.*;
 
 /**
  * @author RJ
@@ -10,4 +14,11 @@ import javax.ws.rs.Path;
 public interface RootResource {
   @GET
   String root();
+
+  @GET
+  @Path("data/grid.json")
+  @Produces(MediaType.APPLICATION_JSON)
+  String vueComponentsTestData4Grid(@QueryParam("pageNo") int pageNo,
+                                    @QueryParam("pageSize") int pageSize,
+                                    @QueryParam("query") String query);
 }
--- simter-demo-rest-jaxrs/src/main/java/tech/simter/demo/rest/jaxrs/RootResourceImpl.java
+++ simter-demo-rest-jaxrs/src/main/java/tech/simter/demo/rest/jaxrs/RootResourceImpl.java
@@ -13,4 +13,24 @@ public class RootResourceImpl implements RootResource {
   public String root() {
     return "A Simter Demo.";
   }
+
+  @Override
+  public String vueComponentsTestData4Grid(int pageNo, int pageSize, String query) {
+    return "{" +
+      "\"count\":333," +
+
+      "\"columns\":[" +
+      "{\"id\":\"date\",\"label\":\"日期\",\"width\":\"7em\"},{\"id\":\"str\",\"label\":\"文本\",\"width\":\"15em\",\"title\":true}," +
+      "{\"id\":\"money\",\"label\":\"金额（元）\",\"children\":[" +
+      "{\"id\":\"moneyIn\",\"label\":\"支出\",\"width\":\"7em\"}," +
+      "{\"id\":\"moneyOut\",\"label\":\"收入\",\"width\":\"7em\",\"title\":true}]}," +
+      "{\"id\":\"num\",\"label\":\"数字\",\"width\":\"7em\"}" +
+      "]," +
+
+      "\"rows\":[" +
+      "{\"date\":\"2017-5-11\",\"str\":\"文本1\",\"num\":1,\"moneyIn\":100,\"moneyOut\":101.05}," +
+      "{\"date\":\"2017-5-10\",\"str\":\"文本2\",\"num\":2,\"moneyIn\":200,\"moneyOut\":102.05}," +
+      "{\"date\":\"2017-5-9\",\"str\":\"文本3\",\"num\":3,\"moneyIn\":300,\"moneyOut\":103.05}" +
+      "]}";
+  }
 }
